a9d057c89908a662af4f019a8ef52586fe9d756b
AlexanderBraatz <alex.braatz.g@gmail.com>
2025-06-19 13:48:26 +0100

    test

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,5 +1,4 @@
 import { allLinks, homePageLinks, testimonialLinks } from '@/lib/data';
-
 export type SectionName =
 	| (typeof homePageLinks)[number]['name']
 	| (typeof testimonialLinks)[number]['name'];
