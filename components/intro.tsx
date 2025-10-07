211d84ed468e6aeddab7a0ece12cfdf173cacce1
AlexanderBraatz <alex.braatz.g@gmail.com>
2025-10-07 15:07:39 +0100

    updated years experience

--- components/intro.tsx
+++ components/intro.tsx
@@ -77,7 +77,7 @@ export default function Intro() {
 			>
 				<span className="font-bold">Hello, I'm Alexander.</span> I'm a{' '}
 				<span className="font-bold">full-stack developer</span> with{' '}
-				<span className="font-bold">over 2 years</span> of experience. I enjoy
+				<span className="font-bold">over 3 years</span> of experience. I enjoy
 				building <span className="font-bold">user centric </span>
 				<span className="italic">web apps</span>. My focus is{' '}
 				<span className="font-bold">React (Next.js)</span>.
